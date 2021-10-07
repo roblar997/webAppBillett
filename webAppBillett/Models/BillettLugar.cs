4041ad60a99b76454eb7b12c812e4501120761ef
Robert Larsen <robertlarsenn@gmail.com>
2021-10-07 15:34:20 +0200

    public int ruteId { get; set; }         public string avgangsDato { get; set; }
            public string avgangsTid { get; set; } i billettLugar, slik at en kan reservere billett og se om noe er reservert

--- webAppBillett/Models/BillettLugar.cs
+++ webAppBillett/Models/BillettLugar.cs
@@ -21,5 +21,11 @@ namespace webAppBillett.Models
         [System.Text.Json.Serialization.JsonIgnore]
         public virtual Lugar lugar { get; set; }
 
+        //For å hindre at en bestiller lugarer som allerede er tatt
+        public int ruteId { get; set; }
+        public string avgangsDato { get; set; }
+
+        public string avgangsTid { get; set; }
+
     }
 }
