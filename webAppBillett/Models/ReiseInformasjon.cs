7d4a6a100d89730d311288aa48e0e6d100e2d834
Robert Larsen <robertlarsenn@gmail.com>
2021-10-08 17:35:27 +0200

    ...

--- webAppBillett/Models/ReiseInformasjon.cs
+++ webAppBillett/Models/ReiseInformasjon.cs
@@ -10,37 +10,47 @@ namespace webAppBillett.Models
 {
     public class ReiseInformasjon
     {
+        const int INF = 999999;
+
         public ReiseInformasjon()
         {
 
         }
-
+      
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Range(1, INF)]
         public int reiseId { get; set; }
 
 
         [Required]
+        [Range(1, INF)]
         public int fra { get; set; }
 
  
         [Required]
+        [Range(1, INF)]
         public int til { get; set; }
 
 
         [Required]
+        [DataType(DataType.Date)]
         public string avgangsDato { get; set; }
 
 
         [Required]
+        [DataType(DataType.Time)]
         public string avgangsTid { get; set; }
 
 
         [Required]
+        [Range(1, 10)]
         public int antVoksen { get; set; }
 
 
         [Required]
+        [Range(1,10)]
+
         public int antBarn { get; set; }
 
 
