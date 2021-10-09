def6cd08d122f114acbf8c41ff58eb49e8677a3b
Robert Larsen <robertlarsenn@gmail.com>
2021-10-09 09:14:25 +0200

    ..

--- webAppBillett/wwwroot/js/validering.js
+++ webAppBillett/wwwroot/js/validering.js
@@ -57,3 +57,17 @@ function validerPersonSkjema(data) {
 
     return fornavnSjekk && etternavnSjekk && telefonSjekk;
 }
+
+function validerBetalingSkjema(data) {
+    let postNrSjekk = validerPostNr(data.postnr);
+    let postStedSjekk = validerPoststed(data.poststed);
+    let emailSjekk = validerEmail(data.email);
+    let adresseSjekk = validerAdresse(data.adresse);
+
+    let csvSjekk = validerCsv(data.csv);
+    let kortholderNavnSjekk = validerKortholderNavn(data.kortholderNavn);
+    let utlopsTidSjekk = validerUltopsDato(data.utlopsTid);
+    let kortnummerSjekk = validerKortnummer(data.kortnummer);
+
+    return postNrSjekk && postStedSjekk && emailSjekk && adresseSjekk && csvSjekk && kortholderNavnSjekk && utlopsTidSjekk && kortnummerSjekk;
+}
