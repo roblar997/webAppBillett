95657348b5d6ea5baa2065e79a8c6ef29c327541
Robert Larsen <robertlarsenn@gmail.com>
2021-10-29 10:56:47 +0200

    For hvert type kjøretøy har man en antatt maksvekt en ikke tror overskrides.

---
+++ webAppBillett/Models/Kjoretoy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.Threading.Tasks;
+namespace webAppBillett.Models
+{
+    public class Kjoretoy
+    {
+        public Kjoretoy()
+        {
+
+        }
+
+     
+
+        public string typeKjoretoy { get; set; }
+        
+        public int antattMaksVekt { get; set; }
+
+    }
+}
