6723324b35acf0aef2397629a2134b0eec64d218
Robert Larsen <robertlarsenn@gmail.com>
2021-10-29 15:24:39 +0200

    ...

---
+++ webAppBillett/Models/BillettKjoretoy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.Threading.Tasks;
+namespace webAppBillett.Models
+{
+    public class BillettKjoretoy
+    {
+        public BillettKjoretoy()
+        {
+
+        }
+
+        public virtual Billett billett{ get; set; }
+        public virtual Kjoretoy kjoretoy { get; set; }
+
+        public bool harVåpen { get; set; }
+        public bool harElAparat { get; set; }
+        public bool harSproyteBeholder { get; set; }
+        public bool harGassBeholder { get; set; }
+
+        public int antKjæledyr { get; set; }
+
+        public string infoInnhold { get; set; }
+    }
+}
