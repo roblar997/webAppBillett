1835e27c0d7dc3afd6bff44bcf04910b2e7c5f17
Robert Larsen <robertlarsenn@gmail.com>
2021-10-29 10:54:21 +0200

    tenker at hver rute har maks antall for hver av kjøretypene.

--- webAppBillett/Models/Rute.cs
+++ webAppBillett/Models/Rute.cs
@@ -27,6 +27,25 @@ namespace webAppBillett.Models
         [Required]
         public double prisBarn { get; set; }
 
+        public int makspersonBiler { get; set; }
+        public int makspersonbilTilhenger { get; set; }
+
+        public int makslastebil { get; set; }
+
+        public int makslettLastebil { get; set; }
+
+        public int maksmotorsykkel { get; set; }
+
+        public int maksminibuss { get; set; }
+
+
+        public int maksbuss { get; set; }
+
+        public int maksmoped { get; set; }
+
+        public int makstraktor { get; set; }
+        public int makssnoScooter { get; set; }
+
         [ForeignKey("ruteId")]
         public virtual List<RuteForekomstDato> ruteForekomstDato { get; set; }
     }
